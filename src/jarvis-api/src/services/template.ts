a1ba262cbd94ee35c0e71bd688a38a6448e7f20a
Roberto Armas <roberto.armas@verndale.com>
2024-03-02 14:42:03 -0500

    fix

--- src/jarvis-api/src/services/template.ts
+++ src/jarvis-api/src/services/template.ts
@@ -11,23 +11,24 @@ export const getTemplates = async () => {
     );
 
     const query = gql`
-    query getTemplates {
-        customDatasourceTemplates: search(query: {
-            searchStatement: {
-                criteria:[
-                    {
-                        operator:MUST,field:"__base_template_sm", value:"166a7c8b6a8c4c6db1b0f46c27ba22fc"
-                    }, 
-                    {
-                        operator: MUST, field:"_language", value:"en"
+        query getTemplates {
+            customDatasourceTemplates: search(
+                query: {
+                    searchStatement: {
+                        criteria: [
+                            {
+                                operator: MUST
+                                field: "__base_template_sm"
+                                value: "166a7c8b6a8c4c6db1b0f46c27ba22fc"
+                            }
+                            { operator: MUST, field: "_language", value: "en" }
+                        ]
                     }
-                ]
-            }
-        })
-    {
+                }
+            ) {
                 results {
-                name
-                templateId: itemId
+                    name
+                    templateId: itemId
                 }
             }
         }
@@ -51,31 +52,33 @@ export const getTemplate = async (templateId: string) => {
     );
 
     const query = gql`
-    query getTemplateInfo($templateId:ID!,$language: String!){
-        itemTemplate(where:{
-          database:"master",
-          templateId:$templateId
-        }){
-            name
-            templateId
-            ownFields {
-              edges {
-                node {
-                  name
-                  title (language:$language)
-                  section {
-                    name
-                  }
-                  type
+        query getTemplateInfo($templateId: ID!, $language: String!) {
+            itemTemplate(
+                where: { database: "master", templateId: $templateId }
+            ) {
+                name
+                templateId
+                ownFields {
+                    edges {
+                        node {
+                            name
+                            title(language: $language)
+                            section {
+                                name
+                            }
+                            type
+                        }
+                    }
                 }
-              }
             }
         }
-      }
     `;
 
     try {
-        const result = await graphQLClient.request(query, { templateId: templateId, language: "en" });
+        const result = await graphQLClient.request(query, {
+            templateId: templateId,
+            language: "en",
+        });
         return result;
     } catch (e) {
         console.error(e);
