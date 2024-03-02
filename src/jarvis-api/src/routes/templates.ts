f3f2c2ece6dd95b69e5c0defb4849d12978d2b02
Roberto Armas <roberto.armas@verndale.com>
2024-03-02 07:57:21 -0500

    update changes

--- src/jarvis-api/src/routes/templates.ts
+++ src/jarvis-api/src/routes/templates.ts
@@ -1,18 +1,17 @@
-import { Router, Request, Response } from 'express';
-import { Template } from '../models/templates';
+import { Router, Request, Response } from "express";
+import { Template } from "../models/templates";
+import { getTemplates } from "../services/template";
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    let templates: Template[] = [];
-    let template = { 'id': '1', 'name': 'First Template' } as Template;
-    templates.push(template);
+router.get("/", async (req: Request, res: Response) => {
+    const templates = await getTemplates();
     res.json(templates);
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get("/:id", (req: Request, res: Response) => {
     let templateId = req.params.id;
-    let template = { 'id': templateId, 'name': 'First Template' };
+    let template = { id: templateId, name: "First Template" };
     res.json(template);
 });
 
